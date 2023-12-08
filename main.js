f06a9082a9cfe06b71084bec24a507a237d7945b
JackStendeback <jackstendeback@gmail.com>
2023-12-07 21:15:48 -0600

    added comments, and a dedication to my grandma.

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ function generatePoem() {
             let title = data[0].title;
             let author = data[0].author;
 
-            // Check if the poem is below a certain length
+            // * Check if the poem is below a certain length
             if (poem.split(' ').length <= 100) { 
                 document.getElementById('poem-text').textContent = poem;
                 document.getElementById('poem-title').textContent = title;
                 document.getElementById('poem-author').textContent = author;
             } else {
-                // If the poem is too long, generate a new one
+                // * If the poem is too long, generate a new one
                 generatePoem();
             }
         })
@@ -25,15 +25,15 @@ function generatePoem() {
 }
 
 function savePoem() {
-    // Get the current poem, title, and author
+    // * Get the current poem, title, and author
     const poem = document.getElementById('poem-text').textContent;
     const title = document.getElementById('poem-title').textContent;
     const author = document.getElementById('poem-author').textContent;
 
-    // Get the existing poems from localStorage
+    // * Get the existing poems from localStorage
     const savedPoems = JSON.parse(localStorage.getItem('savedPoems')) || [];
 
-    // Add the new poem to the array
+    // * Add the new poem to the array
     savedPoems.push({ title, author, poem });
 
     // Save the updated array back to localStorage
@@ -41,13 +41,13 @@ function savePoem() {
 }
 
 window.onload = function() {
-    // Check localStorage for any saved poems
+    // * Check localStorage for any saved poems
     const savedPoems = JSON.parse(localStorage.getItem('savedPoems')) || [];
 
-    // Format each poem and join them into a single string
+    // * Format each poem and join them into a single string
     const formattedPoems = savedPoems.map(formatPoem).join('');
 
-    // Display the saved poems
+    // * Display the saved poems
     document.getElementById('saved-poems').innerHTML = formattedPoems;
 };
 
